54cd42811a31a4d666727375ba7477ee58a24e34
Amin <amin.sh35322088@gmail.com>
2026-09-04 04:58:26 +0330

    chore(model-center): remove obsolete model selection shim

--- src/bot/callbacks/model-selection-callback-handler.ts
+++
@@ -1,2 +0,0 @@
-// Compatibility export only. Model selection UI and callbacks are implemented exclusively by Model Center.
-export { handleModelSearchTextInput } from "./model-center-callback-handler.js";
